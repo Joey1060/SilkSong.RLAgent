ecdfd2ad5768bd2438606b0d3a478b0a2be15095
Joey <z5548349@ad.unsw.edu.au>
2025-10-19 20:21:51 +1100

    feat(Models): add an eval mod for actor

--- Model/actor.py
+++ Model/actor.py
@@ -3,6 +3,7 @@ import torch
 import os
 import json
 import uuid
+import sys
 import re
 import socket
 import json
@@ -94,6 +95,12 @@ def no_opposites(actions):
     left, right, up, down = actions[:,0], actions[:,1], actions[:,2], actions[:,3]
     return (left + right <= 1) & (up + down <= 1)
 
+
+training_mode = True
+if (len(sys.argv) == 2):
+    if (sys.argv[1] == '--eval'):
+        training_mode = False
+
 space = ReducedBinaryActionSpace(n_bits=4, constraints=no_opposites)
 
 # Agent and buffer
@@ -106,31 +113,51 @@ cur_checkpoint = None
 
 dumper = EpisodeDumper()
 
+if training_mode:
+    while (True):
+        latest_checkpoint = agent.find_latest_checkpoint()
+        # if (latest_checkpoint is None):
+        #     time.sleep(5)
+        #     continue
+        if (latest_checkpoint != cur_checkpoint):
+            print(f"new checkpoint found: {latest_checkpoint}, loading....")
+            agent.load_checkpoint(latest_checkpoint)
+            cur_checkpoint = latest_checkpoint
 
-while (True):
-    latest_checkpoint = agent.find_latest_checkpoint()
-    # if (latest_checkpoint is None):
-    #     time.sleep(5)
-    #     continue
-    if (latest_checkpoint != cur_checkpoint):
-        print(f"new checkpoint found: {latest_checkpoint}, loading....")
-        agent.load_checkpoint(latest_checkpoint)
-        cur_checkpoint = latest_checkpoint
-
-    episode_data = []
-    next_state = env.reset()
-    done = False
-    while (not done):
-        # print("?")
-        agent.reset_noise()
-        _, action = agent.select_action(next_state, space)
-        action_list = action.numpy()
-        action_mask = 0
-        for i in range(len(action_list)):
-            action_mask = action_mask | ((1 & action_list[i]) << i)
-        # print(action_list, action_mask)
-        prev_state, prev_action, reward, next_state, done = env.step(action_mask)
-        episode_data.append([prev_state, space.action_to_index(action), reward, next_state, done])
-    print(f"episode finished, dumping data....")
-    dumper.dump(episode_data)
+        episode_data = []
+        next_state = env.reset()
+        done = False
+        while (not done):
+            # print("?")
+            agent.reset_noise()
+            _, action = agent.select_action(next_state, space)
+            action_list = action.numpy()
+            action_mask = 0
+            for i in range(len(action_list)):
+                action_mask = action_mask | ((1 & action_list[i]) << i)
+            # print(action_list, action_mask)
+            prev_state, prev_action, reward, next_state, done = env.step(action_mask)
+            episode_data.append([prev_state, space.action_to_index(action), reward, next_state, done])
+        print(f"episode finished, dumping data....")
+        dumper.dump(episode_data)
+else:
+    with torch.no_grad():
+        latest_checkpoint = agent.find_latest_checkpoint()
+        if (latest_checkpoint is not None):
+            agent.load_checkpoint(latest_checkpoint)
+        agent.q_net.eval()
+        next_state = env.reset()
+        done = False
+        while (not done):
+            # print("?")
+            agent.reset_noise()
+            _, action = agent.select_action(next_state, space)
+            print(action)
+            action_list = action.numpy()
+            action_mask = 0
+            for i in range(len(action_list)):
+                action_mask = action_mask | ((1 & action_list[i]) << i)
+            # print(action_list, action_mask)
+            prev_state, prev_action, reward, next_state, done = env.step(action_mask)
+        print(f"episode finished")
 env.close()
